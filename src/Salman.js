dfecdae240d32e185968ef28cb3295f15b5c0175
MA-27231 <mashhadia640@gmail.com>
2024-10-06 21:46:02 +0330

    first commit

---
+++ src/Salman.js
@@ -0,0 +1,10 @@
+import Avatar from "./components/Avatar.jsx";
+import "./Salman.css";
+const Salman = () => {
+  return (
+    <div className="Salman">
+      <h1 id="demo"><Avatar/> S </h1>
+    </div>
+  );
+};
+export default Salman;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import reactDom from "react-dom/client";
+import Salman from "./Salman";
+const root = reactDom.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Salman />
+  </React.StrictMode>
+);
